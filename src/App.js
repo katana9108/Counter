fb58391ececf12f051e742e916e07e1091609bf2
Jeffry Gayvin N'some Minko <jeffrygayvynnsomeminko@gmail.com>
2023-05-06 21:37:46 +0200

    commit

---
+++ src/App.js
@@ -0,0 +1,36 @@
+import { useState } from 'react';
+import './App.css';
+
+function App() {
+
+
+  const [counter,setCounter]=useState(0);
+
+  const handleClickIncrement=(e)=>{
+    e.preventDefault();
+    setCounter(counter+1);
+  }
+
+  const handleClickDecrement=(e)=>{
+    e.preventDefault();
+    counter ===0? setCounter(0):setCounter(counter-1);
+  }
+  const handleClickZero=(e)=>{
+    e.preventDefault();
+    setCounter(0);
+  }
+
+  return (
+
+    <div className="App">
+      <div>{counter}</div>
+      <button onClick={handleClickIncrement}>Increment</button>
+      <button onClick={handleClickDecrement}>Decrement</button>
+      <button onClick={handleClickZero}>Reset To Zero</button>
+
+    </div>
+    
+  );
+}
+
+export default App;
